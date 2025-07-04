10173c4d05d60efaff7aec183c40bba95696128e
xzc <xingzhicheng@imudges.com>
2025-07-04 20:50:20 +0800

    搭建SpringBoot+Mybatis框架

---
+++ src/main/java/com/xzc/gamestore/GameStoreApplication.java
@@ -0,0 +1,16 @@
+package com.xzc.gamestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xzc
+ */
+@SpringBootApplication
+public class GameStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameStoreApplication.class, args);
+	}
+
+}
